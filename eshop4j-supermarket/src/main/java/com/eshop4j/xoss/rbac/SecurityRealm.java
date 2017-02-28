582123309927bfae5ea815a5636568eb5aff2e0f
Mignet <MignetWee@gmail.com>
2017-02-28 11:36:30 +0800

    log

--- eshop4j-supermarket/src/main/java/com/eshop4j/xoss/rbac/SecurityRealm.java
+++ eshop4j-supermarket/src/main/java/com/eshop4j/xoss/rbac/SecurityRealm.java
@@ -1,5 +1,8 @@
 package com.eshop4j.xoss.rbac;
 
+import javax.annotation.Resource;
+
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
@@ -10,6 +13,10 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.eshop4j.core.security.SHA256;
+import com.eshop4j.web.model.User;
+import com.eshop4j.web.service.UserService;
 
 /**
  * 用户身份验证,授权 Realm 组件
@@ -22,12 +29,16 @@ public class SecurityRealm extends AuthorizingRealm {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SecurityRealm.class);
 	
+	@Resource
+    private UserService userService;
+	
     /**
      * 权限检查
      */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
         SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        //没有任何其他权限
         return authorizationInfo;
     }
 
@@ -36,8 +47,16 @@ public class SecurityRealm extends AuthorizingRealm {
      */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token){
-        String username = String.valueOf(token.getPrincipal());
+    	String username = String.valueOf(token.getPrincipal());
         String password = new String((char[]) token.getCredentials());
+        // 通过数据库进行验证
+        final User authentication = userService.authentication(new User(username, SHA256.crypt(password)));
+        if (authentication == null) {
+            throw new AuthenticationException("用户名或密码错误.");
+        }
+        if (!"Y".equals(authentication.getState())) {
+        	throw new AuthenticationException("当前用户无效.");
+        }
         return new SimpleAuthenticationInfo(username, password, getName());
     }
 
